2ed74883ee3b9704b5c03c4df0694f3b3676ebd2
SergeySobolev90krak <sergey.sobolev90@gmail.com>
2022-04-08 15:25:40 +0300

    add new code

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+string[] GetNewArray(string[] firstArray)
+{
+    string[] newArray = new string[firstArray.Length];
+    int size = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            newArray[size] = firstArray[i];
+            size++;
+        }
+    }
+    Array.Resize(ref newArray, size);
+    return newArray;
+}
+void PrintArray(string[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+string[] firstArray = { "Fytmkkrr", "dkgjdkgj",  ",!", "Hello", "111", "yes",  "hard", "$" };
+string[] newArray = GetNewArray(firstArray);
+Console.WriteLine("Массив строк:");
+PrintArray(firstArray);
+Console.WriteLine();
+Console.WriteLine("Новый массив:");
+PrintArray(newArray);
